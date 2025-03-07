9218253d5cc728a8d6ecab076e5ada243d6ae9cc
m-priya-671 <priyamadhan671@gmail.com>
2025-03-07 14:26:07 +0530

    Add files via upload
    task3 finished

---
+++ Calculator/script.js
@@ -0,0 +1,24 @@
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+
+function deleteLast() {
+    let current = document.getElementById("display").value;
+    document.getElementById("display").value = current.slice(0, -1);
+}
+
+
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
